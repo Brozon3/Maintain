7ae7340dae98df66295619e7b77b79545909d8d7
Justin Kawaja <jkawaja909@gmail.com>
2023-11-03 18:19:08 -0230

    DynamoDB test work

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,37 @@
 import express from "express";
-
 const app = express();
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'properties';
+
+AWS.config.update({ region: 'us-east-1' })
+
+
+
+const port = 3000;
+
 app.use(express.json());
+
+app.get("/rows/all", (req, res) => {
+  var params = {
+      TableName: tableName
+  };
+
+  client.scan(params, (err, data) => {
+      if (err) {
+          console.log(err);
+      } else {
+          var items = [];
+          for (var i in data.Items)
+              items.push(data.Items[i]['Name']);
+
+          res.contentType = 'application/json';
+          res.send(items);
+      }
+  });
+});
 
 app.get("/api/hello/:name", (req, res) => {
   const { name } = req.params;
@@ -12,6 +42,6 @@ app.post("/api/hello", (req, res) => {
   res.send(`Hello ${req.body.name}!`);
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
