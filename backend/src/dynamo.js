aa0b7c7aeef783b2e6ad91c6f3d9559de09b2259
Justin <jkawaja909@gmail.com>
2023-11-04 14:45:19 -0230

    updated testing table, added proper import statements

--- backend/src/dynamo.js
+++ backend/src/dynamo.js
@@ -1,38 +1,39 @@
-const AWS = require('aws-sdk'); 
-require('dotenv').config();
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION,
   accesKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
-const TABLE_NAME = 'properties';
+const TABLE_NAME = "users";
 
 const getProperties = async () => {
   const params = {
-    TableName: TABLE_NAME
+    TableName: TABLE_NAME,
   };
   const characters = await dynamoClient.scan(params).promise();
-  console.log(characters)
+  console.log(characters);
   return characters;
-}
+};
 
 const addOrUpdateProperties = async (character) => {
   const params = {
     TableName: TABLE_NAME,
-    Item: character
-  }
+    Item: character,
+  };
   return await dynamoClient.put(params).promise();
-}
+};
 
-//getProperties();
+getProperties();
 
 const property = {
-  "id": "0",
-  "name": "123 Fake Street",
-  "color": "Purple"
-}
+  id: 0,
+  name: "123 Fake Street",
+  color: "Purple",
+};
 
-addOrUpdateProperties(property);
+//addOrUpdateProperties(property);
