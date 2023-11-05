82e07c6d80bfa04a5cfce5fd14bf19df98af008d
Justin Kawaja <jkawaja909@gmail.com>
2023-11-05 15:03:51 -0330

    Added comments, renamed variables to properties from testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 const app = express();
-// import AWS from "aws-sdk";
 import { getAllProperties, getSinglePropertyByID, insertProperty, deleteSinglePropertyById } from "./Commands.js";
 
 const port = 3000;
 
 app.use(express.json());
 
-// const DocumentClient = new AWS.DynamoDB.DocumentClient();
+//Database format
+// {"id": 4, "color":"Green", "name":"This is a real street"}
+
 const TABLE_NAME = 'users';
 
-app.get('/items', async (req, res) => {
+//Get all the properties
+app.get('/properties', async (req, res) => {
 	try {
 		const properties = await getAllProperties(TABLE_NAME);
 		res.status(200).json(properties);
@@ -19,39 +21,39 @@ app.get('/items', async (req, res) => {
 		res.status(err.statusCode || 500).json({ message: err.message || 'Something went wrong' });
 	}
 });
-
-app.post('/items', async (req, res) => {
+// use postman to add a new property
+app.post('/properties', async (req, res) => {
 	const body = req.body;
 	try {
 		const newProperty = await insertProperty(TABLE_NAME, body);
-		console.log('newItem', newProperty);
+		console.log('newProperty', newProperty);
 		res.status(200).json(body);
 	} catch (err) {
 		console.error(err);
 		res.status(err.statusCode || 500).json({ message: err.message || 'Something went wrong' });
 	}
 });
-
-app.get('/items/:id', async (req, res) => {
+// Either run database or run postman to get the property
+app.get('/properties/:id', async (req, res) => {
 	let id = req.params.id;
   id = parseInt(id)
   
 	try {
-		const item = await getSinglePropertyByID(TABLE_NAME, id);
-		res.status(200).json(item);
+		const property = await getSinglePropertyByID(TABLE_NAME, id);
+		res.status(200).json(property);
 	} catch (err) {
 		console.error(err);
     console.log("Is this an int: " + Number.isInteger(id))
 		res.status(err.statusCode || 500).json({ message: err.message || 'Something went wrong' });
 	}
 });
-
-app.delete('/items/:id', async (req, res) => {
+// Use postman to delete a single item by id
+app.delete('/properties/:id', async (req, res) => {
 	let id = req.params.id;
   id = parseInt(id)
 	try {
-		const item = await deleteSinglePropertyById(TABLE_NAME, id);
-		res.status(200).json(item);
+		const property = await deleteSinglePropertyById(TABLE_NAME, id);
+		res.status(200).json(property);
 	} catch (err) {
 		console.error(err);
 		res.status(err.statusCode || 500).json({ message: err.message || 'Something went wrong' });
