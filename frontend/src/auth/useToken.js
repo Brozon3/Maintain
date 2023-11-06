785eff7bdd63896fc567bf2e7efe8298ca44813b
Bard of Code <harnettmusic@gmail.com>
2023-11-06 10:29:36 -0330

    Switch to cookies for tioken storage

--- frontend/src/auth/useToken.js
+++ frontend/src/auth/useToken.js
@@ -2,11 +2,28 @@ import { useState } from "react";
 
 export const useToken = () => {
   const [token, setTokenInternal] = useState(() => {
-    return localStorage.getItem("token");
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith("token=")) {
+        return cookie.substring("token=".length);
+      }
+    }
+    return null;
+    // return localStorage.getItem("token");
   });
 
   const setToken = (newToken) => {
-    localStorage.setItem("token", newToken);
+    // localStorage.setItem("token", newToken);
+    const secureCookie = true;
+    // Currently stopping login process. Investigate and make true
+    const httpOnlyCookie = false;
+
+    const cookieOptions = `path=/;${secureCookie ? "secure;" : ""}${
+      httpOnlyCookie ? "HttpOnly;" : ""
+    }`;
+
+    document.cookie = `token=${newToken}; ${cookieOptions}`;
     setTokenInternal(newToken);
   };
 
