6e0212be52f783d7daddf7dca99cc34b57b9889f
Pat Broders <patrick,broders73@ed.cna.nl.ca>
2023-11-07 20:17:23 -0330

    added modals, fixed swtiches, table form, styling

--- frontend/src/components/AddProperty.js
+++ frontend/src/components/AddProperty.js
@@ -1,73 +1,88 @@
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
 
 export const AddProperty = () => {
 
-  return (
-    <Container className="container w-75 main" >
-        <h1 className='mb-3 blue-header'>Add a Property</h1>
-        <Form>
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Property Address: </Form.Label>
-                <Form.Control type="address" placeholder="20 Fake Pl." />
-            </Form.Group>
+    const { register, handleSubmit } = useForm();
 
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Property City: </Form.Label>
-                <Form.Control type="city" placeholder="Exampleville" />
-            </Form.Group>
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Property Province: </Form.Label>
-                <Form.Select>
-                    <option value={"AB"}>AB</option>
-                    <option value={"BC"}>BC</option>
-                    <option value={"MB"}>MB</option>
-                    <option value={"NL"}>NL</option>
-                    <option value={"NS"}>NS</option>
-                    <option value={"NT"}>NT</option>
-                    <option value={"NU"}>NT</option>
-                    <option value={"ON"}>ON</option>
-                    <option value={"PE"}>PE</option>
-                    <option value={"QC"}>QC</option>
-                    <option value={"SK"}>SK</option>
-                    <option value={"YT"}>YT</option>
-                </Form.Select>
-            </Form.Group>
+    return (
+        <Container className="container main" >
+            <h1 className='mb-3 blue-header'>Add a Property</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='address'>Property Address: </Form.Label>
+                    <Form.Control type="address" placeholder="20 Fake Pl." id="address" {...register("address", { required: true })}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='city'>Property City: </Form.Label>
+                    <Form.Control type="city" placeholder="Exampleville" id="city" {...register("city", { required: true })}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='province'>Property Province: </Form.Label>
+                    <Form.Select id="province" {...register("province", { required: true })}>
+                        <option value={"AB"}>AB</option>
+                        <option value={"BC"}>BC</option>
+                        <option value={"MB"}>MB</option>
+                        <option value={"NL"}>NL</option>
+                        <option value={"NS"}>NS</option>
+                        <option value={"NT"}>NT</option>
+                        <option value={"NU"}>NT</option>
+                        <option value={"ON"}>ON</option>
+                        <option value={"PE"}>PE</option>
+                        <option value={"QC"}>QC</option>
+                        <option value={"SK"}>SK</option>
+                        <option value={"YT"}>YT</option>
+                    </Form.Select>
+                </Form.Group>
 
 
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Property Type: </Form.Label>
-                <Form.Select>
-                    <option value={"Apartment"}>Apartment</option>
-                    <option value={"Cabin"}>Cabin</option>
-                    <option value={"Commercial"}>Commercial</option>
-                    <option value={"Condo"}>Condo</option>
-                    <option value={"Home"}>Home</option>
-                </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='type'>Property Type: </Form.Label>
+                    <Form.Select id="type" {...register("type", { required: true })}>
+                        <option value={"Apartment"}>Apartment</option>
+                        <option value={"Cabin"}>Cabin</option>
+                        <option value={"Commercial"}>Commercial</option>
+                        <option value={"Condo"}>Condo</option>
+                        <option value={"Home"}>Home</option>
+                    </Form.Select>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Carpet: </Form.Label>
-                <Form.Select>
-                    <option value={"No"}>No</option>
-                    <option value={"Yes"}>Yes</option>
-                </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='carpet'>Carpet: </Form.Label>
+                    <Form.Select id="carpet" {...register("carpet", { required: true })}>
+                        <option value={"No"}>No</option>
+                        <option value={"Yes"}>Yes</option>
+                    </Form.Select>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label className='blue-text'>Pets: </Form.Label>
-                <Form.Select>
-                    <option value={"No"}>No</option>
-                    <option value={"Yes"}>Yes</option>
-                </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='pets'>Pets: </Form.Label>
+                    <Form.Select id="pets" {...register("pets", { required: true })}>
+                        <option value={"No"}>No</option>
+                        <option value={"Yes"}>Yes</option>
+                    </Form.Select>
+                </Form.Group>
 
-            <Button type="submit" className='green-button'>
-                Submit
-            </Button>
-        </Form>
-    </Container>
-  )
+                <Form.Group className="mb-3">
+                    <Form.Label className='blue-text' htmlFor='heating'>Heating Type: </Form.Label>
+                    <Form.Select id="heating" {...register("heating", { required: true })}>
+                        <option value={"Electric"}>Electric</option>
+                        <option value={"Oil"}>Oil</option>
+                    </Form.Select>
+                </Form.Group>
+
+                <Button type="submit" className='green-button'>
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    )
 }
--- frontend/src/components/ViewProperties.js
+++ frontend/src/components/ViewProperties.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Card, Button, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const maxProperties = 3;
@@ -9,9 +9,15 @@ export function DisplayProperties({properties}){
     const navigate = useNavigate();
     const addProperty = () => navigate('/addProperty');
     const viewProperty = (id) => navigate('/taskList/' + id);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleOpen = () => setShow(true);
+
     
     return(
-        <Container className="container w-75 main">
+        <Container className="container main">
             <Row xs={1} md={2} className="g-4">
                 {properties.map(property => {
                     return(
@@ -19,17 +25,34 @@ export function DisplayProperties({properties}){
                             <Card className="m-5 text-center blue-border">
                                 <Card.Body className="align-items-center">
                                     <Card.Title className="blue-header">{property.address}</Card.Title>
-                                    <Card.Text className="blue-header">
-                                        {(property.city) + ", " + (property.province)}
+                                    <Card.Text className="blue-secondary-header">
+                                        {property.city}
+                                    </Card.Text>
+                                    <Card.Text className="blue-secondary-header">
+                                        {property.province}
                                     </Card.Text>
                                     <Button type="submit" className="mx-2 green-button" onClick={() => viewProperty(property.id)}>
                                         View Property
                                     </Button>
-                                    <Button type="submit" className="mx-2 green-button">
+                                    <Button type="submit" className="mx-2 green-button" onClick={handleOpen} >
                                         Delete Property
                                     </Button>
                                 </Card.Body>
                             </Card>
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title className="blue-text">Delete Property</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body className="blue-text">Are you sure you want to delete this property?</Modal.Body>
+                                <Modal.Footer>
+                                <Button className="blue-button"  onClick={handleClose}>
+                                    Cancel
+                                </Button>
+                                <Button className="green-button" onClick={handleClose}>
+                                    Delete Property
+                                </Button>
+                                </Modal.Footer>
+                            </Modal>
                         </Col>
                     )
                 })}
