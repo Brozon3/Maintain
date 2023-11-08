02f5ed7d7f476ef89d2757baec8d8179e34f6afb
Pat Broders <patrick,broders73@ed.cna.nl.ca>
2023-11-08 11:59:34 -0330

    fixed merge errors plus edits to existing pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,26 +8,22 @@ import { PropertyTaskList } from "./components/PropertyTaskList";
 import "./index.css";
 import { AddTask } from "./components/AddTask";
 import { useEffect, useState } from "react";
-import { useEffect, useState } from "react";
 import data from "./data/dummyProperties.json";
 import { PrivateRoute } from "./auth/PrivateRoute";
-import {
-  EmailVerificationFail,
-  EmailVerificationSuccess,
-  EmailVerification,
-  LoginPage,
-  PleaseVerifyEmailPage,
-  SignUpPage,
-} from "./pages";
+import { EmailVerificationFail } from "./pages/EmailVerificationFail";
+import { EmailVerificationSuccess } from "./pages/EmailVerificationSuccess";
+import { EmailVerificationLandingPage } from "./pages/EmailVerificationLandingPage";
+import { LoginPage } from "./pages/LoginPage";
+import { PleaseVerifyEmailPage } from "./pages/PleaseVerifyEmailPage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { HomePage } from "./pages/HomePage";
 
 export function App() {
   const [user, setUser] = useState(null);
 
   const [properties, setProperties] = useState(data);
 
-  useEffect(() => {}, [properties]);
-
-  useEffect(() => {}, [properties]);
+  useEffect(() => {}, [user, properties]);
 
   return (
     <BrowserRouter>
@@ -35,7 +31,7 @@ export function App() {
 
       <Footer />
       <Routes>
-        <Route path="/" element={<DisplayProperties />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/signUpPage" element={<SignUpPage />} />
         <Route element={<PrivateRoute user={user} />}>
@@ -43,10 +39,11 @@ export function App() {
             path="/verifyEmail/:verificationString"
             element={<PleaseVerifyEmailPage />}
           />
-          <Route path="/displayProperties" element={<DisplayProperties />} />
-          <Route path="/addProperty" element={<AddProperty />} />
-          <Route path="/applianceList" element={<PropertyApplianceList />} />
-          <Route path="/taskList" element={<PropertyTaskList />} />
+          <Route path="/displayProperties" element={<DisplayProperties properties={properties}/>} />
+          <Route path="/addProperty" element={<AddProperty properties={properties}/>} />
+          <Route path="/applianceList/:id" element={<PropertyApplianceList properties={properties}/>} />
+          <Route path="/taskList/:id" element={<PropertyTaskList properties={properties}/>} />
+          <Route path="/addTask/:id" element={<AddTask properties={properties} />} />
           <Route path="/pleaseVerify" element={<PleaseVerifyEmailPage />} />
         </Route>
       </Routes>
