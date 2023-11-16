4c7dcfffea49404ef7d45b9fda452f13237f8e8c
Justin Kawaja <jkawaja909@gmail.com>
2023-11-16 19:49:30 -0330

    heavy edits. Get and insert functionality for RDS

--- backend/src/commands/users.js
+++ backend/src/commands/users.js
@@ -1,25 +1,56 @@
-import AWS from "aws-sdk";
+import mysql from "mysql";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-AWS.config.update({
-  region: process.env.AWS_DEFAULT_REGION,
-  accesKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
-export const DocumentClient = new AWS.DynamoDB.DocumentClient();
-export const TABLE_NAME = "users";
+const conn = mysql.createConnection({
+  host: process.env.AWS_RDS_HOST,
+  user: process.env.AWS_RDS_USER,
+  password: process.env.AWS_RDS_PASSWORD
+})
 
 export const getAllUsers = async () => {
-  const params = {
-    TableName: TABLE_NAME,
-  };
-  const items = await DocumentClient.scan(params).promise();
-  console.log(items);
-  return items;
-};
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "SELECT * FROM Maintain_Database.users";
+
+      conn.query(sql, function (err, result) {
+        if (err) {
+          console.error("Error getting user: ", err);
+          reject(err);
+        } else {
+          console.log("Successfully got all users.")
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
+
+
+export const insertNewUser = async (userObject) => {
+  const { email } = userObject;
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "INSERT INTO Maintain_Database.users (email) Values (?)";
+
+      conn.query(sql, [email], function (err, result) {
+        if (err) {
+          console.error("Error inserting user: ", err);
+          reject(err);
+        } else {
+          console.log("User inserted successfully.");
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
 
 export const getUserByEmail = async (email) => {
   const params = {
@@ -30,40 +61,29 @@ export const getUserByEmail = async (email) => {
       ":email": email,
     },
   };
-  console.log("Query params:", params);
-  try {
-    const result = await DocumentClient.query(params).promise();
-    console.log("Query result:", result);
+}
 
-    if (result.Count > 0) {
-      return result.Items[0]; // Match the first user found in the query.
-    } else {
-      return null;
-    }
-  } catch (error) {
-    console.error("Error querying DynamoDB:", error);
-    throw error;
-  }
-};
 
-export const insertUser = async (itemObject) => {
+// Likely to be outsourced & removed.
+export const forgotPasswordCode = async (email, passwordResetCode) => {
   const params = {
-    TableName: TABLE_NAME,
-    Item: itemObject,
-  };
-  const result = await DocumentClient.put(params).promise();
-  console.log(result);
-  return result;
-};
-
-export const deleteSingleUserById = async (TABLE_NAME, id) => {
-  const params = {
-    TableName: TABLE_NAME,
+    TableName: "users",
     Key: {
-      id,
+      email: email,
     },
+    UpdateExpression: "SET passwordResetCode = :newValue",
+    ExpressionAttributeValues: {
+      ":newValue": passwordResetCode,
+    },
+    IndexName: "email-index",
   };
-  return await DocumentClient.delete(params).promise();
+  const response = await DocumentClient.update(params, (err, data) => {
+    if (err) {
+      console.err("User update failed. Error: ", Json.stringify(err));
+    } else {
+      console.log("Password reset updated.");
+    }
+  });
 };
 
 export const updateGoogleUser = async (itemObject) => {
@@ -94,26 +114,4 @@ export const updateGoogleUser = async (itemObject) => {
     console.error("Update user failed", e);
     throw e;
   }
-};
-
-// Likely to be outsourced & removed.
-export const forgotPasswordCode = async (email, passwordResetCode) => {
-  const params = {
-    TableName: "users",
-    Key: {
-      email: email,
-    },
-    UpdateExpression: "SET passwordResetCode = :newValue",
-    ExpressionAttributeValues: {
-      ":newValue": passwordResetCode,
-    },
-    IndexName: "email-index",
-  };
-  const response = await DocumentClient.update(params, (err, data) => {
-    if (err) {
-      console.err("User update failed. Error: ", Json.stringify(err));
-    } else {
-      console.log("Password reset updated.");
-    }
-  });
 };
