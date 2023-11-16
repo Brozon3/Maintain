6d5b21d0646a3bf4b0c4fb9b284347be38e06e58
Justin Kawaja <jkawaja909@gmail.com>
2023-11-16 19:48:02 -0330

    added get and insert functionality to properties

--- backend/src/commands/properties.js
+++ backend/src/commands/properties.js
@@ -6,4 +6,47 @@ const conn = mysql.createConnection({
   host: process.env.AWS_RDS_HOST,
   user: process.env.AWS_RDS_USER,
   password: process.env.AWS_RDS_PASSWORD
-})
+});
+
+export const getAllProperties = async () => {
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "SELECT * FROM Maintain_Database.properties";
+
+      conn.query(sql, function (err, result) {
+        if (err) {
+          console.error("Error getting user: ", err);
+          reject(err);
+        } else {
+          console.log("Successfully got all users.")
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
+
+export const insertProperty = async (userObject) => {
+  const { address, city, prov, type, roof, carpet, pets, heating } = userObject;
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "INSERT INTO Maintain_Database.properties (address, city, prov, type, roof, carpet, pets, heating) Values (?, ?, ?, ?, ?, ?, ?, ?)";
+
+      conn.query(sql, [address, city, prov, type, roof, carpet, pets, heating], function (err, result) {
+        if (err) {
+          console.error("Error inserting user: ", err);
+          reject(err);
+        } else {
+          console.log("User inserted successfully.");
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
