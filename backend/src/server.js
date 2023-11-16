3ad534ab8fd2c74cef9ae706c4cb3b819dd745d8
Justin Kawaja <jkawaja909@gmail.com>
2023-11-16 19:21:57 -0330

    added users, properties api section calls

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,16 @@ import express from "express";
 import { routes } from "./routes/index.js";
 const app = express();
 
+import {
+  getAllUsers,
+  insertNewUser,
+  updateGoogleUser,
+} from "./commands/users.js";
+import {
+  getAllProperties,
+  insertProperty,
+  } from "./commands/properties.js";
+
 const port = 8000;
 
 app.use((req, res, next) => {
@@ -13,6 +23,66 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+// Users Section
+// Get all the users
+app.get("/api/users/", async (req, res) => {
+  console.log("hit");
+  try {
+    const users = await getAllUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: err.message || "Something went wrong" });
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  const body = req.body;
+  try {
+    const newProperty = await insertNewUser(body);
+    console.log("newProperty", newProperty);
+    res.status(200).json(body);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: err.message || "Something went wrong" });
+  }
+});
+
+
+
+//Properties section
+app.get("/api/properties", async (req, res) => {
+  console.log("hit");
+  try {
+    const users = await getAllProperties();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: err.message || "Something went wrong" });
+  }
+});
+
+app.post("/api/properties", async (req, res) => {
+  const body = req.body;
+  try {
+    const newProperty = await insertProperty(body);
+    console.log("newProperty", newProperty);
+    res.status(200).json(body);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(err.statusCode || 500)
+      .json({ message: err.message || "Something went wrong" });
+  }
+});
+
 
 // Add the routes stores in the routes/index.js folder
 routes.forEach((route) => {
