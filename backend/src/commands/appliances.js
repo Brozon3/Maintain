7452192f2ffafd62847be72bfd58dbefe6dbe4a0
Justin Kawaja <jkawaja909@gmail.com>
2023-11-16 21:25:08 -0330

    added getAll, insert, and delete functions to appliances

--- backend/src/commands/appliances.js
+++ backend/src/commands/appliances.js
@@ -15,10 +15,56 @@ export const getAllAppliances = async () => {
 
       conn.query(sql, function (err, result) {
         if (err) {
-          console.error("Error getting user: ", err);
+          console.error("Error getting appliance: ", err);
           reject(err);
         } else {
-          console.log("Successfully got all users.")
+          console.log("Successfully got all appliances.")
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
+
+export const insertAppliance = async (userObject) => {
+  const { type, manufacturer, model, serialNumber, purchaseDate, warrantyLength } = userObject;
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = `INSERT INTO Maintain_Database.appliances 
+      (type, manufacturer, model, serialNumber, purchaseDate, warrantyLength) 
+      Values (?, ?, ?, ?, ?, ?)`;
+
+      conn.query(sql, [type, manufacturer, model, serialNumber, purchaseDate, warrantyLength], function (err, result) {
+        if (err) {
+          console.error("Error inserting user: ", err);
+          reject(err);
+        } else {
+          console.log("User inserted successfully.");
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database: ", error);
+      reject(error);
+    }
+  });
+}
+
+export const deleteAppliance = async (userObject) => {
+  const { applianceID } = userObject;
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = `DELETE FROM Maintain_Database.users WHERE (applianceID) = ?`;
+
+      conn.query(sql, [applianceID], function (err, result) {
+        if (err) {
+          console.error("Error deleting appliance: ", err);
+          reject(err);
+        } else {
+          console.log("Appliance deleted successfully.");
           resolve(result);
         }
       });
