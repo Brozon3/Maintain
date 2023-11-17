c7a26f39a7ded0ae9950f8e27751a009e3260e40
Bard of Code <harnettmusic@gmail.com>
2023-11-17 01:35:51 -0330

    commit

--- backend/src/commands/properties.js
+++ backend/src/commands/properties.js
@@ -7,3 +7,25 @@ const conn = mysql.createConnection({
   user: process.env.AWS_RDS_USER,
   password: process.env.AWS_RDS_PASSWORD,
 });
+
+export const insertNewUser = async (userObject) => {
+  const { email } = userObject;
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "INSERT INTO Maintain_Database.properties (email) VALUES (?)";
+
+      conn.query(sql, [email], function (err, result) {
+        if (err) {
+          console.error("Error inserting user:", err);
+          reject(err);
+        } else {
+          console.log("User inserted successfully");
+          resolve(result);
+        }
+      });
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      reject(error);
+    }
+  });
+};
