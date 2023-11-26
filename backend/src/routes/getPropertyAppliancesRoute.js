4bbefa7bc6456b7ab6fc502d26b4af46cfe39165
Justin Kawaja <jkawaja909@gmail.com>
2023-11-26 17:43:18 -0330

    wrote getPropertyAppliancesRoute code using getPropertyAppliances

---
+++ backend/src/routes/getPropertyAppliancesRoute.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { getPropertyAppliances } from "../commands/properties.js";
+
+export const getPropertyAppliancesRoute = {
+  path: "/api/propertyAppliances/:id",
+  method: "get",
+  handler: async (req, res) => {
+    let id = req.params.id;
+    const getAppliances = await getPropertyAppliances(id);
+    console.log("Get appliances", getAppliances);
+    if (getAppliances.length > 0) {
+      res.status(200).json({ getAppliances });
+    } else {
+      res.status(200).json({ message: "No appliances in list." });
+    }
+  },
+};
