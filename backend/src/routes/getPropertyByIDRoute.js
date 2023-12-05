018fe71a82942a2ff2a7dd7363b65528f69986f8
Pat Broders <patrick.broders73@ed.cna.nl.ca>
2023-12-05 15:36:10 -0330

    fixed property error

--- backend/src/routes/getPropertyByIDRoute.js
+++ backend/src/routes/getPropertyByIDRoute.js
@@ -4,12 +4,10 @@ export const getPropertyByIDRoute = {
   path: "/api/properties/:propertyID",
   method: "get",
   handler: async (req, res) => {
-    console.log("Route gets called.");
 
     let propertyID = req.params.propertyID;
-    console.log(propertyID);
     const propertyResult = await getPropertyByID(propertyID); 
-    console.log(propertyResult)
-    res.status(200).json({ propertyResult });
+    const { propID, address, city, prov } = propertyResult;
+    res.status(200).json( {propertyID: propID, address, city, prov} );
   },
 };
