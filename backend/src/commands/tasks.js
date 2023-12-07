1333c242795bfc9f993a4391ddb036e5c36de0ec
Bard of Code <harnettmusic@gmail.com>
2023-12-07 12:49:56 -0330

    callInsertEventID(entryID, eventID)

--- backend/src/commands/tasks.js
+++ backend/src/commands/tasks.js
@@ -5,8 +5,41 @@ dotenv.config();
 const conn = mysql.createConnection({
   host: process.env.AWS_RDS_HOST,
   user: process.env.AWS_RDS_USER,
-  password: process.env.AWS_RDS_PASSWORD
-})
+  password: process.env.AWS_RDS_PASSWORD,
+});
+
+export const callInsertEventID = async (entryID, eventID) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const sql = "CALL Maintain_Database.insertEventId(?, ?)";
+      conn.query(sql, [
+        entryID,
+        eventID,
+        function (err, result) {
+          if (err) {
+            console.error("Error adding eventID", err);
+            reject(err);
+          } else {
+            const responseParams = "SELECT @message_res AS message_res";
+            conn.query(responseParams, function (err, outputResult) {
+              if (err) {
+                console.error("Message:", err);
+                reject(err);
+              } else {
+                const message = outputResult[0].message_res;
+                console.log("Message:", message);
+                resolve({ message: message });
+              }
+            });
+          }
+        },
+      ]);
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      reject(error);
+    }
+  });
+};
 
 export const callAddTask = async (userID, propertyID, taskObject) => {
   const { description, frequency, dueDate, featureType, applianceType } =
@@ -17,14 +50,21 @@ export const callAddTask = async (userID, propertyID, taskObject) => {
         "CALL Maintain_Database.add_task(?, ?, ?, ?, ?, ?, ?, @message_res)";
       conn.query(
         sql,
-        [description, dueDate, userID, frequency, featureType, applianceType, propertyID],
+        [
+          description,
+          dueDate,
+          userID,
+          frequency,
+          featureType,
+          applianceType,
+          propertyID,
+        ],
         function (err, result) {
           if (err) {
             console.error("Error adding task", err);
             reject(err);
           } else {
-            const responseParams =
-              "SELECT @message_res AS message_res";
+            const responseParams = "SELECT @message_res AS message_res";
             conn.query(responseParams, function (err, outputResult) {
               if (err) {
                 console.error("Error fetching output parameters:", err);
@@ -63,8 +103,7 @@ export const callPropertyTaskView = async () => {
       reject(error);
     }
   });
-}
-
+};
 
 export const callUpdateTask = async (entryID) => {
   return new Promise((resolve, reject) => {
@@ -84,4 +123,4 @@ export const callUpdateTask = async (entryID) => {
       reject(error);
     }
   });
-}
+};
