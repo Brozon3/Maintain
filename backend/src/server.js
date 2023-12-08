5048b98f6e4ccb1b076fe377f58f5b537152f320
Justin Kawaja <jkawaja909@gmail.com>
2023-12-08 17:53:40 -0330

    tried adding oauthClient for experimentation, and port changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,48 @@
 import express from "express";
+import 'dotenv/config';
+import path from "path";
 import { routes } from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { google } from "googleapis";
+
+export const oauthClient = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "http://localhost:8000/auth/google/callback"
+);
+
+export const getGoogleOauthUrl = () => {
+  const scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/calendar",
+  ];
+
+  return oauthClient.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes,
+  });
+};
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-const port = 8000;
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
+routes.forEach((route) => {
+  app[route.method](route.path, route.handler);
+});
 
 app.use((req, res, next) => {
   res.setHeader(
@@ -13,12 +52,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-
-routes.forEach((route) => {
-  app[route.method](route.path, route.handler);
-});
-
-app.listen(port, () => {
-  console.log(`Maintain is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Maintain is listening on port ${PORT}`);
 });
