146f1fcf518be6d6dd2eee67fe7fee9044e81ca5
Justin Kawaja <jkawaja909@gmail.com>
2023-12-08 10:41:35 -0330

    adjusted accordion to have tasks opened

--- frontend/src/pages/ViewProperty.js
+++ frontend/src/pages/ViewProperty.js
@@ -6,7 +6,6 @@ import axios from "axios";
 import { PropertyApplianceList } from "../components/PropertyApplianceList";
 
 export const ViewProperty = () => {
-    
   const { propertyID } = useParams();
 
   const today = new Date();
@@ -14,55 +13,51 @@ export const ViewProperty = () => {
   const [property, setProperty] = useState({});
   const [tasks, setTasks] = useState([]);
   const [appliances, setAppliances] = useState([]);
-  
 
-    useEffect (() => {
-        const fetchProperty = async () => {
-            const result = await axios.get(`/api/properties/${propertyID}`)
-            if (result.data) {
-                setProperty(result.data);
-            } else {
-                setProperty([]);
-            }
-        };
-        fetchProperty();
-    }, [property, propertyID]);
-    
-    return (
-        <>
-            <Container className="text-center main" >
+  useEffect(() => {
+    const fetchProperty = async () => {
+      const result = await axios.get(`/api/properties/${propertyID}`);
+      if (result.data) {
+        setProperty(result.data);
+      } else {
+        setProperty([]);
+      }
+    };
+    fetchProperty();
+  }, [property, propertyID]);
 
-                <h1 className="p-3 mb-3 blue-header">{property.address}</h1>
-                <h2 className="blue-secondary-header">{(property.city) + ", " + (property.prov)}</h2>
+  return (
+    <>
+      <Container className="text-center main">
+        <h1 className="p-3 mb-3 blue-header">{property.address}</h1>
+        <h2 className="blue-secondary-header">
+          {property.city + ", " + property.prov}
+        </h2>
 
-                <Accordion className="green-border p-3">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header className="blue-text">Features</Accordion.Header>
-                        <Accordion.Body>
-                        
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header className="blue-text">Appliances</Accordion.Header>
-                        <Accordion.Body>
-                            <PropertyApplianceList />
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="2">
-                        <Accordion.Header className="blue-text">Tasks</Accordion.Header>
-                        <Accordion.Body>
-                            <PropertyTaskList />
-                        </Accordion.Body>
-                    </Accordion.Item>
-
-
-                </Accordion>
-                <br></br>
-                <br></br>
-                <br></br>
-
-
-            </Container>
-        </>
-    )
-}
+        <Accordion className="green-border p-3" defaultActiveKey={["2"]}>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header className="blue-text">Features</Accordion.Header>
+            <Accordion.Body></Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header className="blue-text">
+              Appliances
+            </Accordion.Header>
+            <Accordion.Body>
+              <PropertyApplianceList />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header className="blue-text">Tasks</Accordion.Header>
+            <Accordion.Body>
+              <PropertyTaskList />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <br></br>
+        <br></br>
+        <br></br>
+      </Container>
+    </>
+  );
+};
