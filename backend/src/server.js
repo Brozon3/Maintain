ed9a86c85be6230e8e50a9cbfb1b0b99d1335321
Justin Kawaja <jkawaja909@gmail.com>
2023-12-09 14:58:38 -0330

    Deleted code no longer needed in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,27 +5,6 @@ import { routes } from "./routes/index.js";
 import { fileURLToPath } from "url";
 import { google } from "googleapis";
 
-export const oauthClient = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:8000/auth/google/callback"
-);
-
-export const getGoogleOauthUrl = () => {
-  const scopes = [
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/calendar",
-  ];
-
-  return oauthClient.generateAuthUrl({
-    access_type: "offline",
-    prompt: "consent",
-    scope: scopes,
-  });
-};
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
