131d2834b87a54709956de9325225b1b16ba7096
Justin Kawaja <jkawaja909@gmail.com>
2023-12-11 16:21:59 -0330

    http to https change

--- backend/src/util/oauthClient.js
+++ backend/src/util/oauthClient.js
@@ -3,5 +3,5 @@ import { google } from "googleapis";
 export const oauthClient = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://www.maintain.lol/api/auth/google/callback"
+  "https://maintain.lol/api/auth/google/callback"
 );
