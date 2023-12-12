8cc665f12896e5e33e71cbc7846fe77b7de1a032
Justin Kawaja <108029557+jkawaja@users.noreply.github.com>
2023-12-12 16:01:06 -0330

    added calendar.js to branch

---
+++ backend/src/commands/calendar.js
@@ -0,0 +1,27 @@
+import { google } from "googleapis";
+import "dotenv/config";
+
+export const listEvents = async (auth) => {
+  const calendar = google.calendar({ version: "v3", auth });
+  const res = await calendar.events.list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.log("No upcoming events found.");
+    return;
+  }
+  console.log("Upcoming 10 events:");
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    console.log(`${start} - ${event.summary} - Event id: ${event.id} `);
+  });
+
+  // authorize().then(listEvents).catch(console.error);
+};
+
+// listEvents(oauthClient);
