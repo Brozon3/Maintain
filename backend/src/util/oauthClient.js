5fb74141ffa9313888f40f89c8e61e9d42a14fcb
Pat Broders <patrick.broders73@ed.cna.nl.ca>
2023-12-12 15:54:07 -0330

    minor changes for task functionality

--- backend/src/util/oauthClient.js
+++ backend/src/util/oauthClient.js
@@ -3,5 +3,5 @@ import { google } from "googleapis";
 export const oauthClient = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://www.maintain.lol/api/auth/google/callback"
+  "http://maintain.lol/api/auth/google/callback"
 );
