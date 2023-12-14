ecb29de9660187429f5a5bec3e7825f962332869
Bard of Code <harnettmusic@gmail.com>
2023-12-14 15:46:49 -0330

    please

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import { PasswordRequirements } from "./auth/PasswordRequirements";
 import { ViewProperty } from "./pages/ViewProperty";
 import { ApplianceForm } from "./pages/AddAppliance";
 
-// Could you?
+// Could you please
 
 export function App() {
   const [user] = useState(null);
