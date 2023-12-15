f1b8a962759f939ed80e4075319847fad8ea429b
Bard of Code <harnettmusic@gmail.com>
2023-12-15 10:55:20 -0330

    pipeline take 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import { PasswordRequirements } from "./auth/PasswordRequirements";
 import { ViewProperty } from "./pages/ViewProperty";
 import { ApplianceForm } from "./pages/AddAppliance";
 
-// Diff build project
+// More settings
 
 export function App() {
   const [user] = useState(null);
